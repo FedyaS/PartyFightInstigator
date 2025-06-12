058e66c7b32c760242629a12fce59034670fcde4
Fedya S <fedsemenov52@gmail.com>
2025-06-12 14:49:43 -0700

    Update GameLayout.jsx

--- frontend/src/components/GameLayout.jsx
+++ frontend/src/components/GameLayout.jsx
@@ -2,12 +2,13 @@ import GameStats from "./GameStats";
 import ConversationView from "./ConversationView";
 import ViewSwitcher from "./ViewSwitcher";
 import AttendeesView from "./AttendeesView";
-import MapView from "./MapView";
+// import MapView from "./MapView"; // MAP functionality commented out for now
 import "./GameLayout.css";
 import { useState } from "react";
 
 function GameLayout({ children }) {
-	const [activeView, setActiveView] = useState("MAP");
+	// const [activeView, setActiveView] = useState("MAP"); // MAP functionality commented out
+	const [activeView, setActiveView] = useState("ATTENDEES"); // Always use ATTENDEES view for now
 	const [activeNpcId, setActiveNpcId] = useState("ceo");
 
 	return (
@@ -21,7 +22,8 @@ function GameLayout({ children }) {
 				{/* Added ConversationView component */}
 				<aside className="graph-section">
 					<ViewSwitcher activeView={activeView} onViewChange={setActiveView} />
-					{activeView === "MAP" ? <MapView /> : <AttendeesView />}
+					{/* {activeView === "MAP" ? <MapView /> : <AttendeesView />} MAP functionality commented out */}
+					<AttendeesView /> {/* Always show AttendeesView for now */}
 				</aside>
 			</div>
 		</div>
